805705268848122b6de8100b7bef821000595d5c
looubna <looubnaenakhli@gmail.com>
2024-05-04 15:38:29 +0400

    implement clerk webhooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables: {colorPrimary: '#624cf5'}
+      variables: {colorPrimary: '#eb9ba9'}
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
