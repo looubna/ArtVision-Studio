c4df0e22cab13cf3dee6b3a8dc2b80cc1911d1ca
looubna <looubnaenakhli@gmail.com>
2024-05-08 15:59:33 +0400

    Update dependencies and fix UI issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -11,7 +12,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "ArtiVision Studio",
+  title: "ArtVision-Studio",
   description: "AI-powered image generator",
 };
 
@@ -22,10 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{
-      variables: {colorPrimary: '#eb9ba9'}
+      variables: { colorPrimary: '#624cf5' }
     }}>
       <html lang="en">
-        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
